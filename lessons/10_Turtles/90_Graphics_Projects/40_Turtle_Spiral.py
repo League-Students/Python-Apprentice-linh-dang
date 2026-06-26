0b2e3a38d9c86e8a632e03710ba6cea6a513c824
League Student <student@jointheleague.org>
2026-06-26 12:35:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/40_Turtle_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/40_Turtle_Spiral.py
@@ -1,51 +1,13 @@
-"""
-# 40_Turtle_Spiral.py
+import turtle 
 
-This program already works. See if you can change it to make it draw a different pattern.
+tina = turtle.Turtle()
 
-uid: rkftzcAi
-name: Turtle Spiral
-"""
+tina.shape("turtle")
 
-import random
-import turtle
+screen = turtle.Screen()
 
-# Returns a random color!
-def get_random_color():
-    return "#%06X" % (random.randint(0, 0xFFFFFF))
-
-window = turtle.Screen()
-window.bgcolor("white")
-
-# Make a new turtle
-my_turtle = turtle.Turtle()
-
-# This code sets our shape to a turtle
-my_turtle.shape("turtle")
-
-# Set your turtle's speed
-my_turtle.speed(0)
-
-# Set your turtle's color
-my_turtle.color("green")
-
-# Use a loop to repeat the code below 50 times
-for i in range(50):
-
-    # Set the turtle color to a random color
-    my_turtle.pencolor(get_random_color())
-
-    # Move the turtle (9*i) pixels. 'i' is the loop variable
-    my_turtle.forward(9 * i)
-
-    # Turn the turtle (360/7 + i*5) degrees to the right
-    my_turtle.right(360 / 7 + i*5)
-
-    # Change the turtle width to 'i' (the loop variable)
-    my_turtle.width(i)
-
-    # Check the pattern against the picture in the recipe. If it matches, you are done.
-
-turtle.done()
-
-# Now check in your code.
+def move_forward():
+  tina.forward(20)
+  
+screen.listen()
+screen.onkey(move_forward, "Up")
